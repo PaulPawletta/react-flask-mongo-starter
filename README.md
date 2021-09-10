785acf9c1e001a89ca92b0285b62ba40b037a680
PaulPawletta <paul.pawletta@gmail.com>
2021-09-10 21:27:59 +0200

    configuring react app and nginx

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ docker build -f Dockerfile.api -t ppawletta/flask-api .
 docker push ppawletta/flask-api
 ```
 
-## 
+## Nginx with React app
+```
+docker build -f Dockerfile.client -t ppawletta/nginx-react .
+docker push ppawletta/nginx-react
+```
--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restx import Api
 from mongoengine import connect
 from v1.resources.todos import todos
@@ -11,6 +11,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Blueprint to change URL base path to /api
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint)
+app.register_blueprint(blueprint)
+
 if str.lower(os.environ['FLASK_ENV']) == 'production':
     app.config.from_object(Production())
     app.config['MONGODB_URL'] = os.environ['MONGODB_URL']
@@ -19,7 +24,6 @@ else:
 
 connect('app', host=app.config['MONGODB_URL'])
 
-api = Api(app)
 api.add_namespace(todos)
 
 if __name__ == "__main__":
